9c2ec0a8d674d43455323c9b3c2466b1307d069d
dylan <dylanhogg@gmail.com>
2021-08-30 23:00:23 +1000

    Update client app

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,15 @@
 var version = "v0.0.1";
 
 $(document).ready( function () {
+    $("#clear-button").click(function(){
+        $("#address-text").val("")
+    });
+
     $("#address-button").click(function(){
         var address = $("#address-text").val();
-        //alert(address);
+
+        $("#response").html("Parsing address...");
+
         $.ajax({
            type: "POST",
            url: "https://address-api.infocruncher.com/predict/",
@@ -11,13 +17,31 @@ $(document).ready( function () {
            success: function(data)
            {
                // alert("success");
-               alert(JSON.stringify(data));
+               // alert(JSON.stringify(data));
+
+               $("#response").html("Success Reponse:<br />" + JSON.stringify(data, null, "  "))
            },
            error: function(data)
            {
-               alert("error");
-               alert(JSON.stringify(data));
+               // alert("error: " + JSON.stringify(data));
+               $("#response").html("Error Reponse:<br />" + JSON.stringify(data, null, "  "))
            },
          });
     });
+
+    $("#response").html("Initialising service...");
+    $.ajax({
+       type: "POST",
+       url: "https://address-api.infocruncher.com/predict/",
+       data: '{"address": "1 Prime Lane"}',
+       success: function(data)
+       {
+           $("#response").html("Success Reponse (Priming):<br />" + JSON.stringify(data, null, "  "))
+       },
+       error: function(data)
+       {
+           // alert("error: " + JSON.stringify(data));
+           $("#response").html("Error Reponse (Priming):<br />" + JSON.stringify(data, null, "  "))
+       },
+     });
 });
