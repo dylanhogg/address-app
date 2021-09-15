e4fac2a4fe5a76ca81465933b7737ff50f4fb8ce
dylan <dylanhogg@gmail.com>
2021-09-15 13:55:40 +1000

    Client updates.

--- client/app/app.js
+++ client/app/app.js
@@ -8,54 +8,64 @@ $(document).ready( function () {
     $("#address-button").click(function(){
         var address = $("#address-text").val();
 
-        $("#response").html("Parsing address...");
+        $("#response").html("Reading address...");
 
         var start = new Date().getTime();
-
         $.ajax({
            type: "POST",
            url: "https://address-api.infocruncher.com/predict/",
            data: '{"address": "'+address+'"}',
+           tryCount : 0,
+           retryLimit : 1,
            success: function(data)
            {
                var end = new Date().getTime();
                var time = end - start;
-               $("#response").html("Clent timing: " + time + "<br />"
+               $("#response").html("Client timing: " + time + "<br />"
+                                                  + "Retries: "+this.tryCount+"<br />"
                                                   + "Success Reponse:<br />"
                                                   + JSON.stringify(data, null, "  "))
            },
            error: function(data)
            {
+               this.tryCount++;
+               if (this.tryCount <= this.retryLimit) {
+                   // Retry
+                   $("#response").html("Retry " + this.tryCount + "...");
+                   $.ajax(this);
+                   return;
+               }
                var end = new Date().getTime();
                var time = end - start;
-               $("#response").html("Clent timing: " + time + "<br />"
+               $("#response").html("Client timing: " + time + "<br />"
+                                                  + "Retries: "+this.tryCount+"<br />"
                                                   + "Error Reponse:<br />"
                                                   + JSON.stringify(data, null, "  "))
            },
          });
     });
 
-    $("#response").html("Initialising service...");
-    var start = new Date().getTime();
-    $.ajax({
-       type: "POST",
-       url: "https://address-api.infocruncher.com/predict/",
-       data: '{"address": "1 Prime Lane"}',
-       success: function(data)
-       {
-            var end = new Date().getTime();
-            var time = end - start;
-            $("#response").html("Clent timing: " + time + " (Priming)<br />"
-                                             + "Success Reponse:<br />"
-                                             + JSON.stringify(data, null, "  "))
-       },
-       error: function(data)
-       {
-           var end = new Date().getTime();
-           var time = end - start;
-           $("#response").html("Clent timing: " + time + " (Priming)<br />"
-                                            + "Error Reponse:<br />"
-                                            + JSON.stringify(data, null, "  "))
-       },
-     });
+//    $("#response").html("Initialising service...");
+//    var start = new Date().getTime();
+//    $.ajax({
+//       type: "POST",
+//       url: "https://address-api.infocruncher.com/predict/",
+//       data: '{"address": "1 Prime Lane"}',
+//       success: function(data)
+//       {
+//            var end = new Date().getTime();
+//            var time = end - start;
+//            $("#response").html("Clent timing: " + time + " (Priming)<br />"
+//                                             + "Success Reponse:<br />"
+//                                             + JSON.stringify(data, null, "  "))
+//       },
+//       error: function(data)
+//       {
+//           var end = new Date().getTime();
+//           var time = end - start;
+//           $("#response").html("Clent timing: " + time + " (Priming)<br />"
+//                                            + "Error Reponse:<br />"
+//                                            + JSON.stringify(data, null, "  "))
+//       },
+//     });
 });
