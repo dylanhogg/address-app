ab29f0626bff8f6a7d67c78930646beb332e8c26
dylan <dylanhogg@gmail.com>
2021-09-16 16:58:40 +1000

    Client updates.

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,55 @@
-var version = "v0.0.1";
+const version = "v0.0.1";
+const predictionUrl = "https://address-api.infocruncher.com/predict/";
+const randomAddresses = [
+        "48a Pirrama Rd Pyrmont NSW 2009",
+        "6/341 George Str, Sydney NSW 2000",
+        "108-110 Kippax Lane, Surry Hills NSW 2010",
+        "L41, Tower Two, 200 Barangaroo Ave, Sydney NSW 2000",
+        "Unit 18/14-18 Flood St, Bondi, NSW 2026",
+        "Aptt 16, 400 Bondi Rd, Bondi NSW 2026",
+        "Lvl 15/333 George St Sydney NSW 2000",
+        "Shop 4/11-31 York St, Sydney NSW 2000",
+        "Floor 3, 152-156 Clarence St, Sydney NSW 2000",
+    ]
+
 var predict_count = 0;
 
-$(document).ready( function () {
+$(document).ready(function () {
+    var randomElement = randomAddresses[Math.floor(Math.random() * randomAddresses.length)];
+    $("#address-text").val(randomElement)
+
     $("#clear-button").click(function(){
         $("#address-text").val("")
+        $("#address-text").focus();
+    });
+
+    $("#random-button").click(function(){
+        var randomElement = randomAddresses[Math.floor(Math.random() * randomAddresses.length)];
+        $("#address-text").val(randomElement)
+    });
+
+    $("#view-debug-info").click(function(){
+        $("#debug-response").toggle();
+    });
+
+    $("#address-text").keypress(function (e) {
+      if (e.which == 13) {
+        $("#address-button").click();
+        return false;
+      }
     });
 
     $("#address-button").click(function(){
+        $("#debug-response").html("");
+
         var address = $("#address-text").val();
+        if (address.length == 0) {
+            $("#response").html("Address is empty.<br />Experiment with the Random button.");
+            return;
+        }
 
         if (predict_count == 0) {
-           $("#response").html("Reading address...<br />(first time can take a few secs)");
+           $("#response").html("Reading address...<br />...and waking up the server");
         } else {
            $("#response").html("Reading address...");
         }
@@ -18,7 +57,7 @@ $(document).ready( function () {
         var start = new Date().getTime();
         $.ajax({
            type: "POST",
-           url: "https://address-api.infocruncher.com/predict/",
+           url: predictionUrl,
            data: '{"address": "'+address+'"}',
            tryCount : 0,
            retryLimit : 1,
@@ -27,51 +66,84 @@ $(document).ready( function () {
                predict_count++;
                var end = new Date().getTime();
                var time = end - start;
-               $("#response").html("Client timing: " + time + "<br />"
-                                                  + "Retries: "+this.tryCount+"<br />"
-                                                  + "Success Reponse:<br />"
-                                                  + JSON.stringify(data, null, "  "))
+
+               var response = "<table class='response'>";
+               var segments = data["result"];
+               for (var key in segments){
+                 var key_display = key.replace(/_/g, " ");
+                 key_display = key_display.charAt(0).toUpperCase() + key_display.slice(1);
+                 response += "<tr><td>" + key_display + "</td><td>" + segments[key] + "</td></tr>";
+               }
+               response += "</table>";
+               $("#response").html(response)
+
+               var debugInfo = "\nDEBUG INFO:\nClient timing: " + time + "\n"
+                                                    + "Retries: "+this.tryCount+"\n"
+                                                    + "Success Reponse:\n"
+                                                    + JSON.stringify(data, null, "  ");
+               console.log(debugInfo);
+               $("#debug-response").html(debugInfo)
            },
            error: function(data)
            {
                this.tryCount++;
                if (this.tryCount <= this.retryLimit) {
-                   // Retry
                    $("#response").html("Retry " + this.tryCount + "...");
                    $.ajax(this);
                    return;
                }
                var end = new Date().getTime();
                var time = end - start;
-               $("#response").html("Client timing: " + time + "<br />"
-                                                  + "Retries: "+this.tryCount+"<br />"
-                                                  + "Error Reponse:<br />"
-                                                  + JSON.stringify(data, null, "  "))
+
+               $("#response").html("Sorry something went wrong, please try again")
+
+               var debugInfo = "\nDEBUG INFO:\nClient timing: " + time + "\n"
+                                            + "Retries: "+this.tryCount+"\n"
+                                            + "Error Reponse:\n"
+                                            + JSON.stringify(data, null, "  ");
+               console.log(debugInfo);
+               $("#debug-response").html(debugInfo)
            },
          });
     });
 
-//    $("#response").html("Initialising service...");
-//    var start = new Date().getTime();
-//    $.ajax({
-//       type: "POST",
-//       url: "https://address-api.infocruncher.com/predict/",
-//       data: '{"address": "1 Prime Lane"}',
-//       success: function(data)
-//       {
-//            var end = new Date().getTime();
-//            var time = end - start;
-//            $("#response").html("Clent timing: " + time + " (Priming)<br />"
-//                                             + "Success Reponse:<br />"
-//                                             + JSON.stringify(data, null, "  "))
-//       },
-//       error: function(data)
-//       {
-//           var end = new Date().getTime();
-//           var time = end - start;
-//           $("#response").html("Clent timing: " + time + " (Priming)<br />"
-//                                            + "Error Reponse:<br />"
-//                                            + JSON.stringify(data, null, "  "))
-//       },
-//     });
+
+    var start = new Date().getTime();
+    $.ajax({
+       type: "POST",
+       url: predictionUrl,
+       data: '{"address": "Page load priming"}',
+       tryCount : 0,
+       retryLimit : 1,
+       success: function(data)
+       {
+            predict_count++;
+            var end = new Date().getTime();
+            var time = end - start;
+            var debugInfo = "\nPRIME SUCCESS:\nClient timing: " + time + "\n"
+                             + "Retries: "+this.tryCount+"\n"
+                             + "Success Reponse:\n"
+                             + JSON.stringify(data, null, "  ");
+            console.log(debugInfo);
+            $("#debug-response").html(debugInfo)
+       },
+       error: function(data)
+       {
+           this.tryCount++;
+           if (this.tryCount <= this.retryLimit) {
+               $("#response").html("Retry " + this.tryCount + "...");
+               $.ajax(this);
+               return;
+           }
+           var end = new Date().getTime();
+           var time = end - start;
+
+           var debugInfo = "\nPRIME ERROR:\nClient timing: " + time + "\n"
+                            + "Retries: "+this.tryCount+"\n"
+                            + "Error Reponse:\n"
+                            + JSON.stringify(data, null, "  ");
+           console.log(debugInfo);
+           $("#debug-response").html(debugInfo)
+       },
+     });
 });
