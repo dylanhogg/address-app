e02b90e45d6f6dd8fe964ce27064c6fc86c620f0
dylan <dylanhogg@gmail.com>
2021-09-19 13:42:46 +1000

    Improve handling of scheduled event.

--- app/app.py
+++ app/app.py
@@ -3,31 +3,17 @@ import uuid
 import boto3
 from typing import Dict
 from datetime import datetime
-from dataclasses import dataclass
+from library.classes import ApiResult, SafeDict
 from addressnet.predict import predict_one
 
 runtime_start = datetime.now()
-app_version = "0.1.15"
+app_version = "1.0.0"
 model_dir = "/opt/ml/model/pretrained"
-dynamodb = boto3.resource('dynamodb')
+allow_origin = "*"   # TODO: lockdown Access-Control-Allow-Origin value below?
+dynamodb_table = "AddressPredictionInferenceFunctionUsage"
 
-
-@dataclass
-class ApiResult:
-    response_body: str = ""
-    address: str = ""
-    ip_address: str = ""
-    user_agent: str = ""
-
-
-class SafeDict(dict):
-    def __missing__(self, key):
-        value = self[key] = type(self)()
-        return value
-
-
-def predict_address(address) -> Dict[str, str]:
-    return predict_one(address, model_dir)
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(dynamodb_table)
 
 
 def handle_api_event(event, handler_start) -> ApiResult:
@@ -61,25 +47,27 @@ def handle_api_event(event, handler_start) -> ApiResult:
     if len(address) > max_address_length:
         raise Exception(f"address length must be less than {max_address_length} chars")
 
-    predict_result = predict_address(address)
+    predict_result = predict_one(address, model_dir)
+    handler_time = str(datetime.now() - handler_start)
+    runtime_time = str(datetime.now() - runtime_start)
     response_body = json.dumps(
         {
             "address": address,
             "result": predict_result,
-            "handler_time": str(datetime.now() - handler_start),
-            "runtime_time": str(datetime.now() - runtime_start),
+            "handler_time": handler_time,
+            "runtime_time": runtime_time,
             "model_dir": model_dir,
             "version": app_version
         })
 
     ip_address = event["requestContext"]["identity"]["sourceIp"]
     user_agent = event["requestContext"]["identity"]["userAgent"]
-    return ApiResult(response_body, address, ip_address, user_agent)
+    return ApiResult(response_body, address, ip_address, user_agent, handler_time, runtime_time)
 
 
 def save_response(request_id, success, api_result, event, response) -> bool:
     try:
-        table = dynamodb.Table("address-api-infocruncher-com-usage")
+        # table = dynamodb.Table(dynamodb_table)
         table.put_item(
             Item={
                 "requestId": request_id,
@@ -88,6 +76,8 @@ def save_response(request_id, success, api_result, event, response) -> bool:
                 "address": api_result.address,
                 "ip_address": api_result.ip_address,
                 "user_agent": api_result.user_agent,
+                "handler_time": api_result.handler_time,
+                "runtime_time": api_result.runtime_time,
                 "response": response,
                 "event": event,
                 "app_version": app_version
@@ -121,14 +111,31 @@ def lambda_handler(event, context) -> Dict[str, str]:
 
     request_id = uuid.uuid4().hex
     handler_start = datetime.now()
-    allow_origin = "*"   # TODO: lockdown Access-Control-Allow-Origin value below?
-    api_result = ApiResult()
+    api_result = None
 
     print(f'INFO: source event: {event}')
     print(f'INFO: source context: {context}')
 
     try:
         event = SafeDict(event)
+
+        if is_scheduled_event(event):
+            scheduled_response = {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "scheduled_event": "True",
+                        "handler_time": str(datetime.now() - handler_start),
+                        "runtime_time": str(datetime.now() - runtime_start),
+                        "model_dir": model_dir,
+                        "version": app_version
+                    }
+                ),
+                "isBase64Encoded": False
+            }
+            print(f'INFO: scheduled_event called')
+            return scheduled_response
+
         api_result = handle_api_event(event, handler_start)
         success_response = {
             "statusCode": 200,
@@ -138,12 +145,13 @@ def lambda_handler(event, context) -> Dict[str, str]:
             },
             "isBase64Encoded": False
         }
-        if not is_scheduled_event(event):
-            print(f'INFO: api_result: [{api_result}]')
-            save_response(request_id, True, api_result, event, success_response)
+
+        print(f'INFO: api_result: [{api_result}]')
+        save_response(request_id, True, api_result, event, success_response)
         return success_response
 
     except Exception as ex:
+        print("ERROR: " + str(ex))
         error_response = {
             "statusCode": 500,
             "body": json.dumps(
@@ -160,8 +168,11 @@ def lambda_handler(event, context) -> Dict[str, str]:
             },
             "isBase64Encoded": False
         }
-        if not is_scheduled_event(event):
-            print("ERROR: " + str(ex))
-            print(f'INFO: error_response: [{error_response}]')
-            save_response(request_id, False, api_result, event, error_response)
+        print(f'INFO: error_response: [{error_response}]')
+
+        if api_result is None:
+            api_result = ApiResult(response_body=error_response,
+                                   handler_time=str(datetime.now() - handler_start),
+                                   runtime_time=str(datetime.now() - runtime_start))
+        save_response(request_id, False, api_result, event, error_response)
         return error_response
